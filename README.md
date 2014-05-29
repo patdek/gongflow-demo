79c4ff416137daac722808d37b70f4911a9563d7
Robert Melton <robert@robertmelton.com>
2014-05-29 05:25:28 -0400

    Basic demo should render properly
    It isn't wired up yet with upload, but I got all the static resources
    in it.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 ngflow-demo
+========================
+This demo users angular, ng-flow-standalone, boostrap
+and the demo application from the ng-flow github repo: 
+https://github.com/flowjs/ng-flow/tree/master/samples/basic
+
+Regenerate static assets
+========================
+This is only needed if you mess with the static assets 
+embedded in the demo, which probably isn't worth it anyway.
+
+prompt: go get https://github.com/jteeuwen/go-bindata
+prompt: $GOPATH/bin/go-bindata html/...
+
+The output will be a fresh new bindata.go with the static
+assets inside of it -- then you can recompile and enjoy.
---
+++ server.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// These assets are embedded into the executable using
+	// https://github.com/jteeuwen/go-bindata
+	// to generate bindata.go -- this removes the need to manage the path
+	// just to run the demo
+	angular, ng, bootstrap, app, index, glyphicons := mustLoadAssets()
+	http.HandleFunc("/angular.min.js", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/javascript")
+		w.Write(angular)
+	})
+	http.HandleFunc("/ng-flow-standalone.min.js", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/javascript")
+		w.Write(ng)
+	})
+	http.HandleFunc("/app.js", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/javascript")
+		w.Write(app)
+	})
+	http.HandleFunc("/bootstrap-combined.min.css", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/css")
+		w.Write(bootstrap)
+	})
+	http.HandleFunc("/img/glyphicons-halflings.png", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "image/png")
+		w.Write(glyphicons)
+	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Write(index)
+	})
+	//http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {  })
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func mustLoadAssets() ([]byte, []byte, []byte, []byte, []byte, []byte) {
+	angular, err := Asset("html/angular.min.js")
+	if err != nil {
+		log.Fatal("Problem loading angular static asset: ", err)
+	}
+	ng, err := Asset("html/ng-flow-standalone.min.js")
+	if err != nil {
+		log.Fatal("Problem loading ng-flow static asset: ", err)
+	}
+	bootstrap, err := Asset("html/bootstrap-combined.min.css")
+	if err != nil {
+		log.Fatal("Problem loading bootstrap static asset: ", err)
+	}
+	app, err := Asset("html/app.js")
+	if err != nil {
+		log.Fatal("Problem loading app static asset: ", err)
+	}
+	index, err := Asset("html/index.html")
+	if err != nil {
+		log.Fatal("Problem loading index.html static asset: ", err)
+	}
+	glyphicons, err := Asset("html/glyphicons-halflings.png")
+	if err != nil {
+		log.Fatal("Problem loading glyphicons static asset: ", err)
+	}
+	return angular, ng, bootstrap, app, index, glyphicons
+}
